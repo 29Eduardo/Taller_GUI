26a645415c6ea8c50892e8e03648d971d218e848
29Eduardo <eduardo.ganchala@epn.edu.ec>
2025-06-24 09:54:09 -0500

    Taller de Navegacion y conexion de ventanas

--- src/FormularioTaller/Registros.java
+++ src/FormularioTaller/Registros.java
@@ -5,4 +5,6 @@ import javax.swing.*;
 public class Registros {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
+    private JTextField textField1;
+    private JComboBox comboBox1;
 }
